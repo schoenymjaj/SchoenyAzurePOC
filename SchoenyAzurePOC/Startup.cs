edc9a791c899a03f3434a263464267a3aa6f4749
schoenymjaj <mns@productivityedge.com>
2014-09-15 14:10:01 -0400

    Initial Comment

---
+++ SchoenyAzurePOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoenyAzurePOC.Startup))]
+namespace SchoenyAzurePOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
